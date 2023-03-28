f3bc7c4b7a00a3d9de288e3e53f2e12ec71949ec
jordbutler <91701088+jordbutler@users.noreply.github.com>
2023-03-28 11:29:23 -0500

    Update testmongo.js

--- testmongo.js
+++ testmongo.js
@@ -35,8 +35,8 @@ console.log("Looking for: " + searchKey);
 
 async function run() {
   try {
-    const database = client.db('ckmdb');
-    const parts = database.collection('cmps415');
+    const database = client.db('jbdb');
+    const parts = database.collection('cmps415mongodb');
 
     // Hardwired Query for a part that has partID '12345'
     // const query = { partID: '12345' };
